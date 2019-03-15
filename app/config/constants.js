58fc65c93bfcb4929d72c4b05a2908699f445e39
Vojislav Stanisic <stanisicvojislav@gmail.com>
2019-03-15 11:56:18 +0100

    add files

---
+++ app/config/constants.js
@@ -0,0 +1,11 @@
+export default {
+  API_URL: 'https://localhost/v1/',
+  PRIMARY_BG_COLOR: '#64672a',
+  PRIMARY_TEXT_COLOR: '#e6e6ff',
+  SECONDARY_BG_COLOR: '#F5FCFF',
+  BAR_STYLE:'light-content',
+  DEFAULT_CITY_IMG: 'https://images.pexels.com/photos/59519/pexels-photo-59519.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb',
+  DEFAULT_HOTEL_IMG: 'https://images.pexels.com/photos/234315/pexels-photo-234315.jpeg?h=350&auto=compress&cs=tinysrgb',
+  PROFILE_AVATAR: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Tom_Hardy_Cannes_2015.jpg/250px-Tom_Hardy_Cannes_2015.jpg',
+  //MAPS_KEY: 'AIzaSyBeqXhZV4hLY-H6coZSQv3c7qwtbjsba6E'
+}
